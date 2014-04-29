26256827b841e71937af2657a00a4aac554e5016
Sean Dokko <sean@hammerquist.net>
2014-04-28 22:01:28 -0700

    implements gatheringLines

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var settings = {
   width: window.innerWidth,
   height: window.innerHeight,
-  radius: 10,
+  radius: 5,
   color: 'white'
 };
 
-var nodes = [];
+var w = 1280,
+    h = 800;
 
-var getRandAngle = function(){
-  return Math.random() * 360;
-  // return Math.sin(angle) * settings.radius;
-};
+var force = d3.layout.force()
+    .gravity(0)
+    .charge(0)
+    .size([w, h]);
 
 var getRandX = function(angle){
   return settings.radius * Math.cos(angle);
@@ -20,24 +21,43 @@ var getRandY = function(angle){
   return settings.radius * Math.sin(angle);
 };
 
-var svg = d3.select('body').append('svg')
-  .attr('class', 'arena')
-  .append('g');
+var getRandAngle = function(){
+  return Math.random() * 360;
+  // return Math.sin(angle) * settings.rad
+};
 
-d3.selectAll('svg.arena').on('mousemove', function(event){
+var svg = d3.select('#container').append('svg:svg')
+  .attr('class', 'arena')
+  .append('g')
+  .attr('width', settings.width)
+  .attr('height', settings.height);
+
+d3.select('svg').on('mousemove', function(event){
   var coord = d3.mouse(this);
   var node = { x: coord[0], y: coord[1] };
+  console.log(node);
+  console.log(force.nodes());
   svg.selectAll('circle')
-    .data([node])
+    .data(force.nodes())
     .enter()
     .append('circle')
     .attr('r', settings.radius)
     .attr('fill', settings.color)
     .attr('cx', function(d) { return Math.random() * settings.width; })
     .attr('cy', function() { return Math.random() * settings.height; })
-    .transition()
-    .duration(1500)
+  .transition()
+    .duration(500)
     .delay(function() { return Math.random() * 1000; })
     .attr('cx', function() { return Math.random() * 10 + coord[0]; })
-    .attr('cy', function() { return Math.random() * 10 + coord[1]; });
+    .attr('cy', function() { return Math.random() * 10 + coord[1]; })
+  .transition()
+    .delay(3000)
+    .attr('r', 1e-6)
+    .each('end', function() {
+      force.nodes().shift();
+    })
+    .remove();
+
+  force.nodes().push(node);
+  force.start();
 });
